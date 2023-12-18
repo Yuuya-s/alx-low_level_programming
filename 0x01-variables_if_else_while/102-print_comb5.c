48245e094962f40c883ed1c144407a147e5a0048
Yuuya-s <147931837+Yuuya-s@users.noreply.github.com>
2023-12-18 23:30:52 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,33 +10,34 @@
  */
 int main(void)
 {
-    int tens1, units1, tens2, units2;
+	int tens1, units1, tens2, units2;
 
-    for (tens1 = 0; tens1 <= 9; tens1++)
-    {
-        for (units1 = 0; units1 <= 9; units1++)
-        {
-            for (tens2 = tens1; tens2 <= 9; tens2++)
-            {
-                for (units2 = (tens2 == tens1) ? units1 + 1 : 0; units2 <= 9; units2++)
-                {
-                    putchar(tens1 % 10 + '0');
-                    putchar(units1 % 10 + '0');
-                    putchar(' ');
-                    putchar(tens2 % 10 + '0');
-                    putchar(units2 % 10 + '0');
+	for (tens1 = 0; tens1 <= 9; tens1++)
+	{
+		for (units1 = 0; units1 <= 9; units1++)
+		{
+			for (tens2 = tens1; tens2 <= 9; tens2++)
+			{
+				for (units2 = (tens2 == tens1) ? units1 + 1 : 0; units2 <= 9; units2++)
+				{
+					putchar(tens1 % 10 + '0');
+					putchar(units1 % 10 + '0');
+					putchar(' ');
+					putchar(tens2 % 10 + '0');
+					putchar(units2 % 10 + '0');
 
-                    if (!(tens1 == 9 && units1 == 8 && tens2 == 9 && units2 == 9))
-                    {
-                        putchar(',');
-                        putchar(' ');
-                    }
-                }
-            }
-        }
-    }
+					if (!(tens1 == 9 && units1 == 8 && tens2 == 9 && units2 == 9))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
 
-    putchar('\n');
+	putchar('\n');
 
-    return 0;
+	return 0;
 }
+
