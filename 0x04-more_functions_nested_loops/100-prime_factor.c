76d61d92a830030ec283e19f9c3e7a55c148ba6a
Yuuya-s <147931837+Yuuya-s@users.noreply.github.com>
2023-12-21 23:16:50 +0300

    Create 100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * largest_prime_factor - finds the largest prime factor of a number.
+ * @n: The number to find the largest prime factor for.
+ * Return: The largest prime factor.
+ */
+long largest_prime_factor(long n)
+{
+    long i;
+
+    for (i = 2; i <= n; i++)
+    {
+        while (n % i == 0)
+        {
+            if (n == i)
+                return i;
+            n /= i;
+        }
+    }
+
+    return 0;
+}
+
+int main(void)
+{
+    long number = 612852475143;
+    long result;
+
+    result = largest_prime_factor(number);
+
+    printf("%ld\n", result);
+
+    return 0;
+}
