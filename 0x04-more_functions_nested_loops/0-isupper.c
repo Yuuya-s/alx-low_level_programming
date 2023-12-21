0e853815f5690bea5776230e885bbd3026115c11
Yuuya-s <147931837+Yuuya-s@users.noreply.github.com>
2023-12-21 22:43:04 +0300

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,7 +7,8 @@
 /**
  * _isupper - checks for uppercase character.
  * @c: The character to be checked.
- * Return: 1 if character is uppercase, 0 otherwise.
+ * Return: 1 if character is uppercase.
+ * 0 otherwise.
  */
 int _isupper(int c)
 {
