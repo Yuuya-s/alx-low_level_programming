ea9568b04d58185f13d42488ea4be395aa5615ab
Yuuya-s <147931837+Yuuya-s@users.noreply.github.com>
2023-12-31 06:34:14 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,37 +1,17 @@
 #include "main.h"
 
 /**
- * _isalpha - checks if a character is an alphabetic letter.
+ * _isalpha - checks if a character is an alphabetic character
  * @c: the character to be checked
  *
- * Return: 1 if c is a letter (lowercase or uppercase), 0 otherwise.
+ * Return: 1 if c is a letter (lowercase or uppercase), 0 otherwise
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1);
-	else
-		return (0);
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+        return 1;
+    else
+        return 0;
 }
 
-/**
- * main - checks the _isalpha function.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int r;
-
-	r = _isalpha('H');
-	_putchar(r + '0');
-	r = _isalpha('o');
-	_putchar(r + '0');
-	r = _isalpha(108);
-	_putchar(r + '0');
-	r = _isalpha(';');
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
-}
 
