9b323808ff715b25815618e4f217a75b5d8d8ff7
Yuuya-s <147931837+Yuuya-s@users.noreply.github.com>
2024-01-02 07:18:49 -0800

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <limits.h>
 
 /**
  * _atoi - Converts a string to an integer.
@@ -12,11 +12,11 @@ int _atoi(char *s)
 	int sign = 1;
 	int i = 0;
 
-	/* Skip leading spaces */
+	/* Skip leading whitespace */
 	while (s[i] == ' ')
 		i++;
 
-	/* Handle optional sign */
+	/* Handle sign */
 	if (s[i] == '-' || s[i] == '+') {
 		sign = (s[i] == '-') ? -1 : 1;
 		i++;
@@ -24,33 +24,16 @@ int _atoi(char *s)
 
 	/* Process digits */
 	while (s[i] >= '0' && s[i] <= '9') {
-		/* Check for overflow */
-		if (result > (2147483647 / 10) || (result == (2147483647 / 10) && s[i] - '0' > 7)) {
-			return (sign == 1) ? 2147483647 : -2147483648;
+		/* Check for overflow before updating result */
+		if (result > INT_MAX / 10 || (result == INT_MAX / 10 && (s[i] - '0') > INT_MAX % 10)) {
+			return (sign == 1) ? INT_MAX : INT_MIN;
 		}
 
+		/* Update result */
 		result = result * 10 + (s[i] - '0');
 		i++;
 	}
 
-	/* Apply sign to result */
-	return sign * result;
-}
-
-/* Example usage */
-int main(void)
-{
-	char str1[] = "123";
-	char str2[] = "   -456";
-	char str3[] = "  +789";
-	char str4[] = "  2147483648";
-	char str5[] = "  -2147483649";
-
-	printf("%d\n", _atoi(str1)); /* Output: 123 */
-	printf("%d\n", _atoi(str2)); /* Output: -456 */
-	printf("%d\n", _atoi(str3)); /* Output: 789 */
-	printf("%d\n", _atoi(str4)); /* Output: 2147483647 (overflow) */
-	printf("%d\n", _atoi(str5)); /* Output: -2147483648 (overflow) */
-
-	return (0);
+	/* Apply sign to the result */
+	return result * sign;
 }
