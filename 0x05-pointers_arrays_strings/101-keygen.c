16844ba4c91104403073a0ad3c293893fb9c3ad9
Yuuya-s <147931837+Yuuya-s@users.noreply.github.com>
2024-01-02 08:00:33 -0800

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,21 +3,19 @@
 #include <time.h>
 
 /**
- * generate_password - Generates a random valid password for 101-crackme.
+ * generate_password - Generates a random valid password.
  *
  * Return: A dynamically allocated string containing the password.
  */
 char *generate_password(void)
 {
-    const char uppercase_chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const char lowercase_chars[] = "abcdefghijklmnopqrstuvwxyz";
-    const char digit_chars[] = "0123456789";
-    
-    const int uppercase_count = sizeof(uppercase_chars) - 1;
-    const int lowercase_count = sizeof(lowercase_chars) - 1;
-    const int digit_count = sizeof(digit_chars) - 1;
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+    const int charset_size = sizeof(charset) - 1;
 
-    const int password_length = 10;
+    const int min_length = 8;
+    const int max_length = 12;
+
+    int password_length = rand() % (max_length - min_length + 1) + min_length;
 
     char *password = (char *)malloc((password_length + 1) * sizeof(char));
     if (password == NULL) {
@@ -28,19 +26,7 @@ char *generate_password(void)
     srand(time(NULL));
 
     for (int i = 0; i < password_length; i++) {
-        int category = rand() % 3;  // 0: uppercase, 1: lowercase, 2: digit
-
-        switch (category) {
-            case 0:
-                password[i] = uppercase_chars[rand() % uppercase_count];
-                break;
-            case 1:
-                password[i] = lowercase_chars[rand() % lowercase_count];
-                break;
-            case 2:
-                password[i] = digit_chars[rand() % digit_count];
-                break;
-        }
+        password[i] = charset[rand() % charset_size];
     }
 
     password[password_length] = '\0'; // Null-terminate the string
@@ -51,7 +37,7 @@ char *generate_password(void)
 int main(void)
 {
     char *password = generate_password();
-    
+
     printf("Generated Password: %s\n", password);
 
     // Don't forget to free the allocated memory
@@ -59,3 +45,4 @@ int main(void)
 
     return 0;
 }
+
