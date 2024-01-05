ea5dc3524dffcbbf17368af8f721ecd36c160473
Yuuya-s <147931837+Yuuya-s@users.noreply.github.com>
2024-01-05 11:49:54 -0800

    Update 0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/*
+ * rot13 - Encodes a string using the ROT13 cipher.
+ * @str: The input string to be encoded.
+ *
+ * Return: A pointer to the encoded string.
+ */
+char *rot13(char *str) {
+    if (str == NULL)
+        return NULL;
+
+    for (; *str; ++str) {
+        if ((*str >= 'A' && *str <= 'Z') || (*str >= 'a' && *str <= 'z')) {
+            if ((*str >= 'A' && *str <= 'M') || (*str >= 'a' && *str <= 'm'))
+                *str += 13;
+            else
+                *str -= 13;
+        }
+    }
+
+    return str;
+}
