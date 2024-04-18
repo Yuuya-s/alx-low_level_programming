078605d229ba7c1c3596ac98e298a88b8058085e
Yuuya-s <147931837+Yuuya-s@users.noreply.github.com>
2024-04-18 19:35:46 +0300

    commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,24 +13,20 @@ hash_table_t *hash_table_create(unsigned long int size)
     hash_node_t **array;
     unsigned long int i;
 
-    // Allocate memory for the hash table structure
     table = malloc(sizeof(hash_table_t));
     if (table == NULL)
         return (NULL);
 
-    // Allocate memory for the array of hash nodes
     array = malloc(sizeof(hash_node_t *) * size);
     if (array == NULL)
     {
-        free(table); // Free previously allocated memory
+        free(table);
         return (NULL);
     }
 
-    // Initialize each element of the array to NULL
     for (i = 0; i < size; i++)
         array[i] = NULL;
 
-    // Initialize hash table structure
     table->array = array;
     table->size = size;
 
