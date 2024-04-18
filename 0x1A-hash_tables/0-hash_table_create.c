8cc3c470b9679acfa109672f735f196de52f0271
Yuuya-s <147931837+Yuuya-s@users.noreply.github.com>
2024-04-18 19:34:43 +0300

    commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,4 +1,5 @@
 #include "hash_tables.h"
+#include <stdlib.h>
 
 /**
  * hash_table_create - creates a hash table with a given size
@@ -8,23 +9,30 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table;
-	hash_node_t **array;
-	unsigned long int i;
+    hash_table_t *table;
+    hash_node_t **array;
+    unsigned long int i;
 
-	table = malloc(sizeof(hash_table_t));
-	if (table == NULL)
-		return (NULL);
+    // Allocate memory for the hash table structure
+    table = malloc(sizeof(hash_table_t));
+    if (table == NULL)
+        return (NULL);
 
-	array = malloc(sizeof(hash_node_t *) * size);
-	if (array == NULL)
-		return (NULL);
+    // Allocate memory for the array of hash nodes
+    array = malloc(sizeof(hash_node_t *) * size);
+    if (array == NULL)
+    {
+        free(table); // Free previously allocated memory
+        return (NULL);
+    }
 
-	for (i = 0; i < size; i++)
-		array[i] = NULL;
+    // Initialize each element of the array to NULL
+    for (i = 0; i < size; i++)
+        array[i] = NULL;
 
-	table->array = array;
-	table->size = size;
+    // Initialize hash table structure
+    table->array = array;
+    table->size = size;
 
-	return (table);
+    return (table);
 }
